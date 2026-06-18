48e03170c2449d1190b0b3c78352e08c66bf110c
saadkhan39 <saadsajid928@gmail.com>
2026-06-18 15:35:51 +0530

    email verification completed

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import userModel from "../models/user.model.js"
 import bcrypt from 'bcryptjs';
 import { sendEmail } from "../service/mail.service.js"
+import jwt from "jsonwebtoken"
 
 //register controller
 export async function register(req,res) {
@@ -30,12 +31,20 @@ export async function register(req,res) {
         password:hash
     })
 
+    const emailVerificationToken = jwt.sign({
+        email:user.email
+    },process.env.JWT_SECRET)
+
+
      await sendEmail({
         to: email,
         subject: "Welcome to Perplexity!",
         html: `
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
+                <p>Please verify your email address by clicking the link below:</p>
+                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
     })
@@ -49,4 +58,45 @@ export async function register(req,res) {
            email:user.email
         }
     })
+}
+
+//verify-email controller
+export async function verifyEmail(req, res) {
+    const { token } = req.query;
+
+    try {
+
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+
+        const user = await userModel.findOne({ email: decoded.email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid token",
+                success: false,
+                err: "User not found"
+            })
+        }
+
+        user.verified = true;
+
+        await user.save();
+
+        const html =
+            `
+        <h1>Email Verified Successfully!</h1>
+        <p>Your email has been verified. You can now log in to your account.</p>
+        <a href="http://localhost:3000/login">Go to Login</a>
+    `
+
+        return res.send(html);
+    } catch (err) {
+        return res.status(400).json({
+            message: "Invalid or expired token",
+            success: false,
+            err: err.message
+        })
+    }
 }
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import express from "express"
 import { registerValidator } from "../validators/auth.validators.js"
-import { register } from "../controllers/auth.controller.js"
+import { register ,verifyEmail} from "../controllers/auth.controller.js"
 
 const authRouter = express.Router()
 
 authRouter.post("/register",registerValidator,register)
+
+authRouter.get("/verify-email",verifyEmail)
 
 
 
