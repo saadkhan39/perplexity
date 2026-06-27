b9663ea0f4f44504bf69bedb38c815a40a66a54b
saadkhan39 <saadsajid928@gmail.com>
2026-06-28 01:18:32 +0530

    READY FOR DEPLOYMENT

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,9 +4,15 @@ import cookieParser from "cookie-parser"
 import morgan from "morgan"
 import cors from "cors"
 import chatRoutes from "./routes/chat.routes.js"
+import path from "path"
+import { fileURLToPath } from "url";
 
 const app = express()
-  
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
@@ -20,6 +26,13 @@ app.use(cors({
 app.use("/api/auth",authRoutes)
 app.use("/api/chats",chatRoutes)
 
+app.get("/{*splat}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
+// React fallback (LAST)
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
 
 export default app
